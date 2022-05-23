2d3254a7710bc71cfb5ad4ce37acd4c4e0b4b5e3
nicole <nicolenganjiaxian@gmail.com>
2022-05-23 20:40:08 +0800

    l

--- staycation.py
+++ staycation.py
@@ -1,4 +1,31 @@
-class staycation:
+def all_records():
+    pass
+
+
+def bubble_sort():
+    pass
+
+
+def selection_sort():
+    pass
+
+
+def insertion_sort():
+    pass
+
+
+def linear_search():
+    pass
+
+
+def binary_search():
+    pass
+
+
+def list_range():
+    pass
+
+class Staycation:
     def __init__(self):
         self.__pkg_name = ""
         self.__cust_name = ""
@@ -7,7 +34,7 @@ class staycation:
 
     @property
     def pkg_name(self) -> str:
-        return self.__pkg_name
+        return self.__pkg_name  
 
     @property
     def cust_name(self) -> str:
@@ -37,25 +64,14 @@ class staycation:
     def cost(self, value: float):
         self.__cost = value
 
-    def all_records():
-        pass
+def print_menu():
+    print("""1. Display all records
+2. Sort record by Customer Name using Bubble sort
+3. Sort record by Package Name using Selection sort
+4. Sort record by Package Cost using Insertion sort
+5. Search record by Customer Name using Linear Search and update record
+6. Search record by Package Name using Binary Search and update record
+7. List records range from $X to $Y. e.g $100-200
+Exit Application""")
 
-    def bubble_sort():
-        pass
-
-    def selection_sort():
-        pass
-
-    def insertion_sort():
-        pass
-
-    def linear_search():
-        pass
-
-    def binary_search():
-        pass
-    
-    def list_range():
-        pass
-
-
+print_menu()
