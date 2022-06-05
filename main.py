1eaca1a1d6c72b4aebb332cfe6d3dc9c56af3385
nic <nicolenganjiaxian@gmail.com>
2022-06-05 21:16:59 +0800

    done!

--- main.py
+++ main.py
@@ -1,59 +1,77 @@
-"""
-extra feature: heap sort/jump search?
-to do: change data_list after sorted, error handling, optimise bubble sort and linear search?
-https://stackabuse.com/search-algorithms-in-python/
-https://stackabuse.com/sorting-algorithms-in-python/
-"""
-
 from staycation import Staycation
 from method import *
 
-c1 = Staycation("Deluxe", "Johnny John", 2, 200)
-c2 = Staycation("Single", "Betty Bett", 1, 150)
-c3 = Staycation("Family", "May Flower", 2, 450)
-c4 = Staycation("Deluxe", "Bobby Bob", 2, 200)
-c5 = Staycation("Suite", "Alexis Alex", 2, 600)
-c6 = Staycation("Single", "Paris Pear", 1, 150)
-c7 = Staycation("Suite", "Jane Joules", 2, 600)
-c8 = Staycation("Family", "Ben Dover", 2, 450)
-c9 = Staycation("Single", "Mike Hawk", 1, 150)
-c10 = Staycation("Single", "Hugh Jass", 1, 150)
+def list_all():
+    for x in data_list:
+        x.return_string()
+        
+        
+c1 = Staycation("Johnny John", "Deluxe", 7, 300)
+c2 = Staycation("Johnny John", "Single",  9, 150)
+c3 = Staycation("May Flower", "Family", 5, 450)
+c4 = Staycation("Bobby Bob", "Ultra Deluxe", 8, 400)
+c5 = Staycation("Alexis Alex", "Suite", 6, 600)
+c6 = Staycation("Paris Pear", "Large Single", 11, 200)
+c7 = Staycation("Jane Joules", "Single Suite", 1, 350)
+c8 = Staycation("Ben Dover", "Big Family", 3, 500)
+c9 = Staycation("Mike Hawk", "Holiday", 2, 550)
+c10 = Staycation("Hugh Jass", "Deluxe Suite", 4, 800)
 
 data_list = [c1, c2, c3, c4, c5, c6, c7, c8, c9, c10]
 
-choice = print_menu()
+while True:
+    choice = print_menu().lower()
+    if choice == "1":
+        list_all()
+        print("Successful sort!")
+            
+            
+    # option 2, sort record by customer name using bubble sort
+    elif choice == "2":
+        bubble_sort(data_list)
+        list_all()
+        print("Successful sort!")
 
-if choice == "1":
-    for x in data_list:
-        x.list_all()
-    # for x in data_list:
-    #     print(x.cust_name)
-    #     print(data_list.index(x))
+    # option 3 sort record by package name using selection sort
+    elif choice == "3":
+        selection_sort(data_list)
+        list_all()
+        print("Successful sort!")
+
+    # option 4 sort record by package cost using insertion sort
+    elif choice == "4":
+        insertion_sort(data_list)
+        list_all()
+        print("Successful sort!")
+
+    # option 5 search record by customer name using linear search and update record
+    elif choice == "5":      
+        linear_search(data_list, validate_str("Enter customer name"))
+        # list_all() 
+
+    # option 6 search record by package name using binary search and update record by
+    elif choice == "6":
+                
+        binary_search(data_list, 0, len(data_list)-1, validate_str("Enter package name"))
+        # list_all()
+
+    elif choice == "7":
         
-# option 2, sort record by customer name using bubble sort
-elif choice == "2":
-   print(bubble_sort(data_list))
-
-# option 3 sort record by package name using selection sort
-elif choice == "3":
-    print(selection_sort(data_list))
-    for x in data_list:
-        x.list_all()
-
-# option 4 sort record by package cost using insertion sort
-elif choice == "4":
-    print(insertion_sort(data_list))
-
-# option 5 search record by customer name using linear search and update record
-elif choice == "5":
-    linear_search()
-
-# option 6 search record by package name using binary search and update record by
-elif choice == "6":
-    binary_search()
-
-elif choice == "7":
-    list_range()
-    
-else:
-    print
+        start = validate_int("Enter start range $")
+        stop = validate_int("Enter stop range $")
+        
+        list_range(data_list, start, stop)
+        
+    elif choice == "8":
+        
+        heap_sort(data_list)
+        list_all()
+        break
+        
+    elif choice == "q":
+        
+        break
+        
+    else:
+        print("\nOption not found, please try again.\n")
+        pass
--- staycation.py
+++ staycation.py
@@ -1,25 +1,40 @@
 class Staycation:
-    def __init__(self, pkg_name, cust_name, no_pax, cost):
-        self.__pkg_name = pkg_name
+    def __init__(self, cust_name, pkg_name, pax, cost):
         self.__cust_name = cust_name
-        self.__no_pax = no_pax
+        self.__pkg_name = pkg_name
+        self.__pax = pax
         self.__cost = cost
-
+        
+    @property
+    def cust_name(self) -> str:
+        return self.__cust_name
     @property
     def pkg_name(self) -> str:
         return self.__pkg_name  
 
     @property
-    def cust_name(self) -> str:
-        return self.__cust_name
-
-    @property
-    def no_pax(self) -> int:
-        return self.__no_pax
+    def pax(self) -> int:
+        return self.__pax
 
     @property
     def cost(self) -> float:
         return self.__cost
     
-    def list_all(self):
-        return print(f"Package Name: {self.__pkg_name}\nCustomer Name: {self.__cust_name}\nPax: {self.__no_pax}\nCost: {self.__cost}\n")
+    @cust_name.setter
+    def cust_name(self, value: str):
+        self.__cust_name = value
+        
+    @pkg_name.setter
+    def pkg_name(self, value: str):
+        self.__pkg_name = value
+
+    @pax.setter
+    def pax(self, value: int):
+        self.__pax = value
+
+    @cost.setter
+    def cost(self, value: float):
+        self.__cost = value
+    
+    def return_string(self):
+        return print(f"Customer Name: {self.__cust_name}\nPackage Name: {self.__pkg_name}\nPax: {self.__pax}\nCost: ${self.__cost}\n")
